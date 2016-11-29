c3aadfa5b11592c517a6aaf1a5db9b6c7dccbe9c
verdejavic <verdejavic@ufl.edu>
2016-11-29 18:58:06 -0500

    add repo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('#navmusic').click(function(){
+    window.location = '#homeMusic';
+});
+
+$('#navvideos').click(function(){
+    window.location = '#homeVideos';
+});
+
+$('#navtour').click(function(){
+    window.location = '#homeTour';
+});
+
+$('#navmedia').click(function(){
+    window.location = '#homeMedia';
+});
