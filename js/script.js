23539eeb975122d470e0c3bb92991a980cbe9cc9
verdejavic <verdejavic@ufl.edu>
2016-12-20 17:54:32 -0500

    12/20

--- js/script.js
+++ js/script.js
@@ -6,10 +6,42 @@ $('#navvideos').click(function(){
     window.location = '#homeVideos';
 });
 
-$('#navtour').click(function(){
-    window.location = '#homeTour';
+$('#navshows').click(function(){
+    window.location = '#homeShows';
 });
 
-$('#navmedia').click(function(){
-    window.location = '#homeMedia';
+$('#logo').click(function(){
+    window.location = '#headerpic';
+});
+
+$('#navmerch').click(function(){
+     window.open('http://www.thesocialguerrillas.com/category/casaveda');
+});
+
+$('#EPshirtimg').click(function(){
+     window.open('http://www.thesocialguerrillas.com/category/casaveda');
+});
+
+$('#shortstories').click(function(){
+     window.open('https://itunes.apple.com/in/album/short-stories-ep/id1181559434');
+});
+
+$('#labyrinths').click(function(){
+     window.open('https://itunes.apple.com/in/album/labyrinths-and-limousines/id998356687');
+});
+
+$('#fbicon').click(function(){
+     window.open('https://www.facebook.com/CasavedaBand/');
+});
+
+$('#instaicon').click(function(){
+     window.open('https://www.instagram.com/casaveda/');
+});
+
+$('#twittericon').click(function(){
+     window.open('https://twitter.com/Casaveda');
+});
+
+$('#youtubeicon').click(function(){
+     window.open('https://www.youtube.com/channel/UCOFh4nmuBLsPVh3VBtrqj-w');
 });
